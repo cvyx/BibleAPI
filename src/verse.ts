42e90d6b47b5b15df44635f13a74b15167d18835
cvyx <162769716+cvyx@users.noreply.github.com>
2024-03-12 14:31:26 +0800

    Added Split Parameter
    With the split parameter, you may now be able to split multiple verse input to an array of verses.
    
    ## Example
    
    ```javascript
    console.log(await getVerse('John', '1:1-3', "NLT", true))
    ```
    
    ```json
    {
      citation: "John 1:1-3 NLT",
      passage: [ "Prologue: Christ, the Eternal Word", " In the beginning the Word already existed.The Word was with God,and the Word was God.",
        " He existed in the beginning with God.", " God created everything through him,and nothing was created except through him."
      ],
    }
    ```

--- src/verse.ts
+++ src/verse.ts
@@ -1,13 +1,13 @@
 import axios from 'axios';
 import cheerio from 'cheerio';
 
-export async function getVerse(book: string, passage: string, version: string = "NLT") {
+export async function getVerse(book: string, passage: string, version: string = "NLT", split: boolean = false) {
     const url = `https://www.biblegateway.com/passage/?search=${book}%20${passage}&version=${version}`;
     const response = await axios.get(url);
     const $ = cheerio.load(response.data);
 
 
-    const passageContent = $('meta[property="og:description"]').attr('content');
+    let passageContent: any = $('meta[property="og:description"]').attr('content');
 
     // Scrape the footnotes
     let footnotes = '';
@@ -24,6 +24,17 @@ export async function getVerse(book: string, passage: string, version: string = 
             "message": `Could not find passage ${book} ${passage} ${version}`
         };
     }
+
+    if (split) {
+        let collected: string[] = [];
+        $(`span[class^="text ${book}-${passage.split(":")[0]}"]`).text().split(/\d+/).forEach(i => {
+            collected.push(i);
+        });
+        passageContent = collected;
+    }
+    
+
+    
  
     return footnotes
         ? { 'citation': `${book} ${passage} ${version}`, 'passage': passageContent, "footnotes": footnotes }
